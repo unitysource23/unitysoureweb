0adb08c2e120ad15e82bf14c3f460559fef15257
Eikhaingsoe1996 <eikhaingsoe.unitysource@gmail.com>
2023-10-26 15:52:22 +0630

    tailwind set up finished

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,8 +6,9 @@ DOCTYPE html>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    @vite('resources/css/app.css')
 </head>
-<body>
-<h1>home</h1>
+<body class="font-jakarta">
+<h1 class="text-primary">home</h1>
 </body>
 </html>
