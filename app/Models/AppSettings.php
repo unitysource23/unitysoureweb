e6b185f8ae9db7011625831375a40abe9a9a4104
ThiSann <thisann7@gmail.com>
2023-10-26 16:52:21 +0630

    add check one row exists

--- app/Models/AppSettings.php
+++ app/Models/AppSettings.php
@@ -8,5 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class AppSettings extends Model
 {
     use HasFactory;
+
     protected $fillable = ['phone_one', 'phone_two', 'phone_three', 'address_one', 'address_two', 'facebook_link', 'instagram_link', 'linkin_link'];
+
+    public function oneRowExists()
+    {
+        return self::exists();
+    }
 }
