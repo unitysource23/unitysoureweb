e88da2b6ddd3e6d0a2372b56a6de2df9a13c9fcd
Eikhaingsoe1996 <eikhaingsoe.unitysource@gmail.com>
2023-11-21 14:38:24 +0630

    swiper js

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -9,7 +9,14 @@
         @vite('resources/css/app.css')
         <link href="{{ asset('css/fontawesome.css') }}" rel="stylesheet">
         <link href="{{ asset('css/home.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/swiper.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/hover.css') }}" rel="stylesheet">
+        <!-- Include compiled CSS -->
+
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
     </head>
+
 
     <body class="font-jakarta">
         <section class="home ">
@@ -116,7 +123,6 @@
                 </div>
             </div>
 
-
             {{--  map two start   --}}
             <div class="mx-10 xl:h-screen mt-10">
                 <div class="grid grid-cols-1 md:grid-cols-3 gap-5">
@@ -184,96 +190,210 @@
                 <div class="grid grid-cols-1 lg:grid-cols-2 gap-10">
                     <div class="relative">
                         <img src="{{ asset('images/Ellipse 12.png') }}" alt="">
-                        <img src="{{ asset('images/Frame.png') }}" class="absolute w-[1300px] top-0 " alt="">
+                        <div
+                            class="bg-gray-300 w-full h-full xl:w-[550px] xl:h-[400px] rounded-lg shadow-xl absolute top-28 left-8">
+                        </div>
+                        <img src="{{ asset('images/smart-life.png') }}"
+                            class="absolute top-32 w-[530px] h-[370px] left-10" alt="">
+                        <div class="flex items-center gap-2 absolute top-[117px] left-10">
+                            <div class="bg-gray-600 w-2 h-2 rounded-full"></div>
+                            <div class="bg-gray-600 w-2 h-2 rounded-full"></div>
+                            <div class="bg-gray-600 w-2 h-2 rounded-full"></div>
+                        </div>
                     </div>
                     <div class="flex items-center">
-                        <div>
+                        <div class="text-center lg:text-left">
                             <h1 class="font-bold text-lg lg:text-2xl">Smart Life</h1>
-                        <h1 class="font-semibold text-primary text-base lg:text-xl mb-5">Hire Purchase Services</h1>
-                        <h1 class="text-paraColor text-sm md:text-base text-justify mb-5" >
-                            Our platform offers a effective solution to effortlessly
-                            manage customer payments and monitor installment
-                            plans. By providing businesses with powerful solution,
-                            we simplify the process of keeping tabs on customer
-                            payments and overseeing installment schedules.
-                            Our user-friendly interface enhances operational
-                            efficiency, allowing organizations to optimize their
-                            workflows and achieve the best possible outcomes.
-                            With our innovative platform, businesses can experience
-                            smoother operations, improved financial management,
-                            and enhanced customer satisfaction.
-                        </h1>
-                        <a href="" class="text-primary font-semibold">Fint Out More <i class="fa-solid fa-arrow-right-long ml-2"></i></a>
+                            <h1 class="font-semibold text-primary text-base lg:text-xl mb-5">Hire Purchase Services
+                            </h1>
+                            <h1 class="text-paraColor text-sm md:text-base text-justify mb-5">
+                                Our platform offers a effective solution to effortlessly
+                                manage customer payments and monitor installment
+                                plans. By providing businesses with powerful solution,
+                                we simplify the process of keeping tabs on customer
+                                payments and overseeing installment schedules.
+                                Our user-friendly interface enhances operational
+                                efficiency, allowing organizations to optimize their
+                                workflows and achieve the best possible outcomes.
+                                With our innovative platform, businesses can experience
+                                smoother operations, improved financial management,
+                                and enhanced customer satisfaction.
+                            </h1>
+                            <a href="" class="text-primary font-semibold">Fint Out More <i
+                                    class="fa-solid fa-arrow-right-long ml-2"></i></a>
                         </div>
 
                     </div>
                 </div>
-                {{--  <div class="grid grid-cols-1 lg:grid-cols-2 gap-10">
-                    <div class="lg:col-span-1 mx-5 relative -z-50">
-                        <img src="{{ asset('images/Ellipse 12.png') }}" class="w-[500px] mx-auto lg:m-0"
-                            alt="">
-                    </div>
-                    <div class="lg:col-span-1 mx-5 lg:mx-0 flex items-center justify-center">
-                        <div class="">
-                            <h1 class="font-bold text-xl lg:text-3xl">Smart Life</h1>
-                            <h1 class="text-primary text-md lg:text-xl font-semibold mb-5">Hire Purchases Services
-                            </h1>
-                            <h1 class="text-paraColor text-sm md:text-base text-justify mb-5">Lorem ipsum dolor sit
-                                amet consectetur
-                                adipisicing elit. Quis rem labore enim, atque earum consequuntur nulla quos eaque
-                                magnam
-                                harum dicta veritatis dignissimos quaerat laudantium quisquam delectus incidunt
-                                consequatur laborum.
-                                Odit, tempora corrupti? Quam veniam tenetur laudantium reiciendis doloremque quidem!
-                                Atque ex eos reprehenderit commodi ea. Ab excepturi aut ad, labore eaque aspernatur,
-                                libero dolorem molestias deleniti fugiat minima unde?
-                                Odit labore eaque porro laborum qui quas quasi dignissimos praesentium voluptatum
-                                enim,
-                                inventore officia id aut iure earum dolorum sit quos ea! Vel dolorum exercitationem
-                                itaque doloremque reiciendis delectus ea.\</h1>
-                            <a href="" class="text-primary  font-semibold">Find Out More <i
-                                    class="fa-solid fa-arrow-right"></i></a>
-                        </div>
 
-                    </div>
-                </div>  --}}
             </div>
 
             {{--  fast move start   --}}
 
-            {{--  <div class="container mx-auto">
-                <div class="grid grid-cols-1 lg:grid-cols-2 gap-2  ">
-                    <div class="text-right mx-5 lg:mx-0 flex items-center">
-                        <div>
-                            <h1 class="font-bold text-lg lg:text-3xl">Fast Move</h1>
-                            <h1 class="text-primary text-lg lg:text-xl font-semibold mb-5 ">Distribution System
-                                Software</h1>
-                            <h1 class="text-paraColor text-sm md:text-base ">
-                                Our Software is a tool that enables businesses to efficiently manage their supply
-                                chain
-                                operations. It assists in automating tasks like inventory tracking, order
-                                processing,
-                                and logistics management. This software enhances the distribution process by
-                                optimizing
-                                inventory levels, minimizing order fulfillment time, and improving overall
-                                operational
+            <div class="mx-10  mt-20">
+                <div class="grid grid-cols-1 lg:grid-cols-2 gap-10">
+                    <div class="col-span-1 flex items-center ">
+                        <div class=" text-center lg:text-right">
+                            <h1 class="font-bold text-lg lg:text-2xl">Smart Life</h1>
+                            <h1 class="font-semibold text-primary text-base lg:text-xl mb-5">Hire Purchase Services
+                            </h1>
+                            <h1 class="text-paraColor text-sm md:text-base  mb-5">
+                                Our Software is a tool that enables businesses to efficiently manage their supply chain
+                                operations. It assists in automating tasks like inventory tracking, order processing,
+                                and logistics management. This software enhances the distribution process by optimizing
+                                inventory levels, minimizing order fulfillment time, and improving overall operational
                                 accuracy. Its a crucial asset for businesses aiming to streamline their distribution
                                 networks, reduce costs, and enhance customer satisfaction.
                             </h1>
-                            <a href="" class="text-primary  font-semibold"><i
-                                    class="fa-solid fa-arrow-left-long mr-2"></i>Find Out More </i></a>
+                            <a href="" class="text-primary font-semibold"> <i
+                                    class="fa-solid fa-arrow-left-long mr-2"></i>Find Out More</a>
                         </div>
-
 
                     </div>
                     <div>
                         <img src="{{ asset('images/Fast Move.png') }}" alt="">
                     </div>
-
                 </div>
-            </div>  --}}
+            </div>
 
             {{-- Our Partner start   --}}
+            <div class="mx-5 lg:mx-10 xl:mx-20 mt-20">
+                <h1 class="text-center font-bold text-lg lg:text-2xl mb-7 ">Our Partners</h1>
+                <div class="swiper mySwiper  ">
+                    <div class="swiper-wrapper mx-16 mt-10 ">
+                        <div class="swiper-slide">
+                            <div class="flex items-center gap-4">
+                                <img src="{{ asset('images/ten1.png') }}" class="w-16 h-16" alt="">
+                                <div>
+                                    <h1 class="font-semibold text-lg mb-2">Ten</h1>
+                                    <h1 class="text-sm">E-commerce</h1>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="flex items-center gap-4">
+                                <img src="{{ asset('images/red-hat.png') }}" class="w-16 h-16" alt="">
+                                <div>
+                                    <h1 class="font-semibold text-lg mb-2">Red Hat</h1>
+                                    <h1 class="text-sm">ICT Solution</h1>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="flex items-center gap-4">
+                                <img src="{{ asset('images/infinity.png') }}" class="w-16 h-16" alt="">
+                                <div>
+                                    <h1 class="font-semibold text-lg mb-2">Ten</h1>
+                                    <h1 class="text-sm">E-commerce</h1>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="flex items-center gap-4">
+                                <img src="{{ asset('images/ten1.png') }}" class="w-16 h-16" alt="">
+                                <div>
+                                    <h1 class="font-semibold text-lg mb-2">Ten</h1>
+                                    <h1 class="text-sm">E-commerce</h1>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="flex items-center gap-4">
+                                <img src="{{ asset('images/red-hat.png') }}" class="w-16 h-16" alt="">
+                                <div>
+                                    <h1 class="font-semibold text-lg mb-2">Red Hat</h1>
+                                    <h1 class="text-sm">ICT Solution</h1>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="flex items-center gap-4">
+                                <img src="{{ asset('images/infinity.png') }}" class="w-16 h-16" alt="">
+                                <div>
+                                    <h1 class="font-semibold text-lg mb-2">Ten</h1>
+                                    <h1 class="text-sm">E-commerce</h1>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="flex items-center gap-4">
+                                <img src="{{ asset('images/ten1.png') }}" class="w-16 h-16" alt="">
+                                <div>
+                                    <h1 class="font-semibold text-lg mb-2">Ten</h1>
+                                    <h1 class="text-sm">E-commerce</h1>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="flex items-center gap-4">
+                                <img src="{{ asset('images/red-hat.png') }}" class="w-16 h-16" alt="">
+                                <div>
+                                    <h1 class="font-semibold text-lg mb-2">Red Hat</h1>
+                                    <h1 class="text-sm">ICT Solution</h1>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="flex items-center gap-4">
+                                <img src="{{ asset('images/infinity.png') }}" class="w-16 h-16" alt="">
+                                <div>
+                                    <h1 class="font-semibold text-lg mb-2">Ten</h1>
+                                    <h1 class="text-sm">E-commerce</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="swiper-pagination"></div>
+
+
+                        <div class=" swiper-button-next">
+                            <i class="fa-solid fa-arrow-right-long   "></i>
+                        </div>
+
+                    <div class="swiper-button-prev">
+                        <div>
+                            <i class="fa-solid fa-arrow-left-long"></i>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="text-center mt-5">
+                    <a href="" class=" text-primary">
+                        Expore More <i class="fa-solid fa-arrow-right-long"></i>
+                    </a>
+                </div>
+
+            </div>
+
+            {{--  Our Client start   --}}
+            <div class="mx-10 h-screen mt-14">
+                <h1 class="text-center font-bold text-lg lg:text-2xl mb-7 ">Our Clients</h1>
+                <div class="grid grid-cols-2 gap-10">
+                    <div class="col-span-1">
+                        <div class="relative cursor-pointer img-hover-zoom ">
+                            <img src="{{ asset('images/work.jpg') }}" class="-z-10" alt="">
+                            <div class="w-4/5 bg-primary h-[100px] absolute -bottom-12 left-[10%]  z-50  mx-auto ">
+                                <div class="px-4 py-2">
+                                    <h1 class="text-white  text-xl mb-1 ">
+                                        Fast move Distribution Software
+                                    </h1>
+                                    <h1 class="text-sm text-gray-200">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-span-1">
+                        <div>
+                            <img src="{{ asset('images/home.jpg') }}" class="" alt="">
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
 
 
 
@@ -282,8 +402,46 @@
         <script src="{{ asset('js/fontawesome.js') }}"></script>
         <script src="{{ asset('js/home.js') }}"></script>
         <script src="{{ asset('js/language.js') }}"></script>
+        <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+        <script>
+            var swiper = new Swiper(".mySwiper", {
+                slidesPerView: 4,
+                spaceBetween: 10,
+
+                autoplay: {
+                    delay: 2500,
+                    disableOnInteraction: false,
+                },
+                pagination: {
+                    el: ".swiper-pagination",
+                    clickable: true,
+                },
+                navigation: {
+                    nextEl: ".swiper-button-next",
+                    prevEl: ".swiper-button-prev",
+                },
+                breakpoints: {
+
+                    320: {
+                        slidesPerView: 1,
+                        spaceBetween: 20
+                    },
+
+                    768: {
+                        slidesPerView: 2,
+                        spaceBetween: 30
+                    },
+
+                    1024: {
+                        slidesPerView: 4,
+                        spaceBetween: 40
+                    }
+                },
 
 
+
+            });
+        </script>
     </body>
 
     </html>
