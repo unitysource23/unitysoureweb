6d7fbc1da2aea50fbb0ebae46d69c01e1bd2fe0d
Thant Zin Htun <thantzintun608@gmail.com>
2025-06-11 14:13:50 +0630

    nav-bar active update

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -2,19 +2,22 @@
   <nav class=" py-4 px-4 bg-white md:px-10 text-sm sticky top-0 shadow-sm z-[99999] ">
       <div class="flex items-center justify-between h-9 px-10">
           <div style="display: flex; justify-content: flex-end; align-items: center; padding-right: 50px;">
-            <img src="{{ asset('images/unityLogo.png') }}" style="height: 65px; width: auto;" alt="Unity Logo">
+              <img src="{{ asset('images/unityLogo.png') }}" style="height: 65px; width: auto;" alt="Unity Logo">
           </div>
           <div class=" hidden sm:block ">
               <div class="flex items-center gap-5">
                   <ul class="flex items-center gap-10 text-black text-base font-medium">
                       <li>
-                          <a href="{{ route('home') }}">Home</a>
+                          <a href="{{ route('home') }}"
+                              class="{{ request()->routeIs('home') ? 'text-green-600 font-semibold' : '' }}">Home</a>
                       </li>
                       <li>
-                          <a href="{{ route('partner') }}">Partnership</a>
+                          <a href="{{ route('partner') }}"
+                              class="{{ request()->routeIs('partner') ? 'text-green-600 font-semibold' : '' }}">Partnership</a>
                       </li>
                       <li>
-                          <a href="{{ route('about_us') }}">About</a>
+                          <a href="{{ route('about_us') }}"
+                              class="{{ request()->routeIs('about_us') ? 'text-green-600 font-semibold' : '' }}">About</a>
                       </li>
                   </ul>
               </div>
@@ -25,18 +28,20 @@
       </div>
       <div class=" hidden absolute top-[100%] w-[100%] left-0 z-[9999] bg-white shadow-md" id="additionalFilter">
           <div class="flex flex-col mt-5 gap-5 px-3 pb-3">
-              <ul class="flex flex-col  gap-10 text-black font-semibold">
+              <ul class="flex flex-col gap-10 text-black font-semibold">
                   <li>
-                      <a href="{{ route('home') }}">Home</a>
+                      <a href="{{ route('home') }}"
+                          class="{{ request()->routeIs('home') ? 'text-green-600' : '' }}">Home</a>
                   </li>
                   <li>
-                      <a href="{{ route('partner') }}">Partnership</a>
+                      <a href="{{ route('partner') }}"
+                          class="{{ request()->routeIs('partner') ? 'text-green-600' : '' }}">Partnership</a>
                   </li>
                   <li>
-                      <a href="{{ route('about_us') }}">About</a>
+                      <a href="{{ route('about_us') }}"
+                          class="{{ request()->routeIs('about_us') ? 'text-green-600' : '' }}">About</a>
                   </li>
               </ul>
-
           </div>
       </div>
   </nav>
