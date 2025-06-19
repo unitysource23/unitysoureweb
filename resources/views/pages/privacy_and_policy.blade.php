2164eeab1370fb3bade5b5106a19e8b7f3515c87
Thant Zin Htun <thantzintun608@gmail.com>
2025-06-19 16:33:31 +0630

    privacy update

--- resources/views/pages/privacy_and_policy.blade.php
+++ resources/views/pages/privacy_and_policy.blade.php
@@ -43,10 +43,7 @@
                         of Your information when You use the Service and tells You about Your privacy rights and how the
                         law protects You.</p>
                     <p>We use Your Personal data to provide and improve the Service. By using the Service, You agree to
-                        the collection and use of information in accordance with this Privacy Policy. This Privacy
-                        Policy has been created with the help of the <a
-                            href="https://www.termsfeed.com/privacy-policy-generator/" target="_blank">Privacy Policy
-                            Generator</a>.</p>
+                        the collection and use of information in accordance with this Privacy Policy</a>.</p>
                     <h2>Interpretation and Definitions</h2>
                     <h3>Interpretation</h3>
                     <p>The words of which the initial letter is capitalized have meanings defined under the following
@@ -329,8 +326,6 @@
 
         <div class="footer">
             <div class="container">
-                <p>Generated using <a href="https://www.termsfeed.com/privacy-policy-generator/"
-                        target="_blank">TermsFeed Privacy Policy Generator</a></p>
             </div>
         </div>
 
